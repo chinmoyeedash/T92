898dd5476cd3d72cb9a8ffce87655942a3819fb2
Nalini Rangamani <nalini.rangamani@gmail.com>
2018-01-31 16:53:07 +0530

    second T92

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -12,7 +12,7 @@ def signup_try():
     url1 = "https://auth.course77.hasura-app.io/v1/signup"
 
 # This is the json payload for the query
-    requestPayload = {
+    requestPayload1 = {
         "provider": "username",
         "data": {
             "username": "nalini",
@@ -21,13 +21,13 @@ def signup_try():
     }
 
 # Setting headers
-    headers = {
+    headers1 = {
         "Content-Type": "application/json",
         "Authorization": "Bearer 9b216217fd5be3310585e364902f7ad120f5ad7b0137c825"
     }
 
 # Make the query and store response in resp
-    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+    resp = requests.request("POST", url1, data=json.dumps(requestPayload1), headers=headers1)
     data1 = resp.json()
 # resp.content contains the json response.
     print(json.dumps(data1))
